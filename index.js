8b14299aa4229331305b17bd768ed86a567fd9d7
kokab-shakil <kukab@vortechs.io>
2021-11-23 22:36:18 -0800

    cors

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const nodemailer = require('nodemailer')
 const email = require('./email')
 const app = express();
 const cors = require('cors')
+const corsOptions ={
+    origin:'https://git.heroku.com/civixa-backened.git', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
 const PORT = process.env.PORT || 5000;
 app.use(express.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('' , email)
 app.listen(PORT, () => {
